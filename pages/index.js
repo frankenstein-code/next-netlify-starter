098251845e32c6a26d0370ac9b14698d4370946d
pruthvi <mail2pruthvi22@gmail.com>
2022-04-30 14:32:35 +0530

    test

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my Portfolio!" />
+        <Header title="Welcome to Pruthvi Portfolio!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
